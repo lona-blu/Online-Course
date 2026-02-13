00dbaa4b87decb8132c121c186e7180fc5e6db8b
lona-blu <phosaelona@gmail.com>
2026-02-13 13:42:05 +0200

    Format urlpatterns for better readability

--- urls.py
+++ urls.py
@@ -3,6 +3,12 @@ from . import views
 
 urlpatterns = [
     path('course/<int:course_id>/', views.course_details, name='course_details'),
-    path('submit/<int:course_id>/', views.submit_exam, name='submit_exam'),
-    path('result/<int:submission_id>/', views.show_exam_result, name='show_exam_result'),
+
+    path('submit/<int:course_id>/', 
+         views.submit_exam, 
+         name='submit_exam'),
+
+    path('result/<int:submission_id>/',
+         views.show_exam_result,
+         name='show_exam_result'),
 ]
